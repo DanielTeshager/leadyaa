0868a32d6ea9645a5a7f813fe43201de04a5ed64
Daniel <dwolela@gmail.com>
2022-01-16 16:15:04 +0400

    updated the onload event fetch to access data.contact_list

--- public/js/app.js
+++ public/js/app.js
@@ -11,7 +11,7 @@ document.addEventListener('DOMContentLoaded', function fetchData() {
         //hide loading
         document.querySelector('.loader').style.display = 'none';
         // create a card for each item
-            data.forEach(item => {
+            data.contact_list.forEach(item => {
                 createCard(item);
             });
         })
